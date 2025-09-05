8771eb63116bf097f5ebb99a0c1c4216109b4d10
Sasha <sasavelikorod@gmail.com>
2025-09-06 00:52:03 +0300

    fix

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -2,6 +2,25 @@ import Link from 'next/link';
 import { getMe } from '@/lib/api/serverApi';
 import css from './ProfilePage.module.css';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Your profile',
+  description: 'Your personal profile',
+  openGraph: {
+    title: 'Your profile',
+    description: 'Your personal profile',
+    url: 'https://09-auth-three-taupe.vercel.app/profile',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Your profile',
+      },
+    ],
+  },
+};
 
 const ProfilePage = async () => {
   const user = await getMe();
